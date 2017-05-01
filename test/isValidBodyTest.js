ce6e36f31231b3f24d2521e7fff3f90740d2fb07
sam <samosborn88@gmail.com>
2017-04-30 22:34:47 -0500

    starts updating utils on backend to handle all logic functions

--- test/isValidBodyTest.js
+++ test/isValidBodyTest.js
@@ -7,7 +7,7 @@ var isValidBody = require('../utils/isValidBody');
 var dataColumnObject = require('../data/dataColumnObject');
 var bodyTemplate = require('../data/bodyTemplate');
 
-describe("method: isValidBody tests", function() {
+describe.only("method: isValidBody tests", function() {
 
   it("returns true with bodyTemplate.js file", function() {
     expect(isValidBody(bodyTemplate)).to.be.true;
@@ -20,4 +20,31 @@ describe("method: isValidBody tests", function() {
 
   });
 
+  it("subtracts two strings of ints", function() {
+    expect("1000" - "-10").to.be.a("number").and.to.equal(1010);
+  });
+
+  it("divides two strings of ints", function() {
+    expect("1000" / "500").to.be.a("number").and.to.equal(2);
+  });
+
+  it("multiplies two strings of ints", function() {
+    expect("1000" * "500").to.be.a("number").and.to.equal(500000);
+  });
+
+  it("adds two strings of ints", function() {
+    expect("1000" + "500").to.be.a("string").and.to.equal("1000500");
+  });
+
+  describe("tests parseInt and parseFloat", function() {
+
+    it("parses an int", function() {
+      expect(Number.parseInt("1000", 10)).to.be.a("number").and.to.equal(1000);
+    });
+
+    it("parses a float", function() {
+      expect(Number.parseFloat("100.0123", 10)).to.be.a("number").and.to.equal(100.0123);
+    });
+
+  })
 });
